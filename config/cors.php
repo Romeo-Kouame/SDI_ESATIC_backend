a69a251d0a24520b8af24d4026b7aeb7c97c3258
Kouassi Roxane <rkouassi372@gmail.com>
2026-02-08 22:16:28 +0000

    commit back

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'groupe/*', 'hackathon/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://hackathon26-frontend.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
